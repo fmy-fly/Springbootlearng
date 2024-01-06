c52d3ab0cb481cb68fee5c04a37bba28e8c97537
fmy <1791477899@qq.com>
2024-01-06 12:33:37 +0800

    1月6日上午实现基本页面

---
+++ vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <NavBar></NavBar>
+  
+  <router-view />
+</template>
+
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  name: "App",
+  components: {
+    NavBar
+  }
+
+}
+
+
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ vue/src/components/GameMap.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class = "gamemap">
+    <canvas>
+
+
+    </canvas>
+
+
+  </div>
+</template>
+
+<script>
+import {GameMap} from "@/assets/scripts/GameMap.js"
+export default {
+   
+}
+</script>
+
+<style scoped>
+
+    div.gamemap{
+        width: 100%;
+        height: 100%;
+    }
+
+</style>
